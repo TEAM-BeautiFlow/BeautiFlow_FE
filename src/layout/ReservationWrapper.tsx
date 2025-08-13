30d8326cbbf517e14c78a12efcc6a440a449457b
LeeMiso <dlalth5610@gmail.com>
2025-08-13 23:09:04 +0900

    고객-예약 부분

--- src/layout/ReservationWrapper.tsx
+++ src/layout/ReservationWrapper.tsx
@@ -6,58 +6,62 @@ import type { Reservation } from "../types/reservations";
 const dummyReservations: Reservation[] = [
   {
     reservationId: 1,
-    status: "예약 확정",
-    title: "이달의 아트",
-    shopname: "매장명",
-    name: "시술자명",
-    option: "없음",
-    address:
+    status: "PENDING",
+    shopId: 1,
+    shopName: "매장명",
+    shopImageUrl: "",
+    shopAddress:
       "짧은 주소 (AK플라자 5호)\n매장 주소 또한 최대 2줄을 유지합니다. (36px)",
-    date: "2025.07.06 · 오후 1시",
-  },
-  {
-    reservationId: 2,
-    status: "예약 확인중",
-    title: "이달의 아트",
-    shopname: "매장명",
-    name: "시술자명",
-    option: "없음",
-    address:
-      "짧은 주소 (AK플라자 5호)\n매장 주소 또한 최대 2줄을 유지합니다. (36px)",
-    date: "2025.08.01 · 오후 3시",
-  },
-  {
-    reservationId: 3,
-    status: "시술 완료",
-    title: "이달의 아트",
-    shopname: "매장명",
-    name: "시술자명",
-    option: "없음",
-    address:
-      "짧은 주소 (AK플라자 5호)\n매장 주소 또한 최대 2줄을 유지합니다. (36px)",
-    date: "2025.07.06 · 오후 1시",
-  },
-  {
-    reservationId: 4,
-    status: "취소 완료",
-    title: "이달의 아트",
-    shopname: "매장명",
-    name: "시술자명",
-    option: "없음",
-    address:
-      "짧은 주소 (AK플라자 5호)\n매장 주소 또한 최대 2줄을 유지합니다. (36px)",
-    date: "2025.07.06 · 오후 1시",
-  },
-  {
-    reservationId: 5,
-    status: "노쇼",
-    title: "이달의 아트",
-    shopname: "매장명",
-    name: "시술자명",
-    option: "없음",
-    address:
-      "짧은 주소 (AK플라자 5호)\n매장 주소 또한 최대 2줄을 유지합니다. (36px)",
-    date: "2025.07.06 · 오후 1시",
+    reservationDate: "2025.07.06",
+    startTime: "오후 1시",
+    totalPrice: 110000,
+    totalDurationMinutes: 120,
+    customerName: "고객명",
+    designerId: 2,
+    designerName: "시술자명",
+    requestNotes: "잘부탁드립니다",
+    treatments: [
+      {
+        treatmentName: "이달의 아트",
+        treatmentPrice: 60000,
+        treatmentImageUrls: [
+          "https://bucket-name.s3.ap-northeast-2.amazonaws.com/images/sample-treatment1.jpg",
+          "https://bucket-name.s3.ap-northeast-2.amazonaws.com/images/sample-treatment2.jpg",
+        ],
+        treatmentDurationMinutes: 60,
+      },
+      {
+        treatmentName: "패디큐어",
+        treatmentPrice: 50000,
+        treatmentImageUrls: [
+          "https://bucket-name.s3.ap-northeast-2.amazonaws.com/images/sample-treatment1.jpg",
+          "https://bucket-name.s3.ap-northeast-2.amazonaws.com/images/sample-treatment2.jpg",
+        ],
+        treatmentDurationMinutes: 60,
+      },
+    ],
+    optionGroups: [
+      {
+        groupName: "젤 제거",
+        optionItems: [
+          {
+            itemName: "자샵",
+          },
+        ],
+      },
+      {
+        groupName: "연장",
+        optionItems: [
+          {
+            itemName: "6-10개",
+          },
+        ],
+      },
+    ],
+    styleImageUrls: [
+      "https://bucket-name.s3.ap-northeast-2.amazonaws.com/images/sample-treatment1.jpg",
+      "https://bucket-name.s3.ap-northeast-2.amazonaws.com/images/sample-treatment2.jpg",
+    ],
   },
 ];
 
