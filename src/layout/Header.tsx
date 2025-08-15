bd0afcfeb6039582b2f247c70e8567cd0a7b9be9
LeeMiso <dlalth5610@gmail.com>
2025-08-15 15:26:09 +0900

    header 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,8 +2,8 @@ import Logo from "../assets/Logo.svg";
 
 export default function Header() {
   return (
-    <h1 className="mx-1 h-[101px] px-4 pt-18 pb-10 text-2xl font-bold tracking-tighter text-[var(--color-purple)] transition-colors">
+    <header>
       <img src={Logo} />
-    </h1>
+    </header>
   );
 }
