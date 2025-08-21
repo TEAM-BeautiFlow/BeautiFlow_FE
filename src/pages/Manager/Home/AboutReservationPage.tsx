f3965886937e4b48aea4439c28113716be578d70
LeeMiso <dlalth5610@gmail.com>
2025-08-21 23:18:56 +0900

    홈 채팅 기능 추가

--- src/pages/Manager/Home/AboutReservationPage.tsx
+++ src/pages/Manager/Home/AboutReservationPage.tsx
@@ -94,7 +94,6 @@ function StatusModal({
 export default function AboutReservationPage({
   onBack,
   onStatusChange,
-  onChat,
 }: ReservationDetailPageProps = {}) {
   const navigate = useNavigate();
   const params = useParams();
@@ -286,36 +285,6 @@ export default function AboutReservationPage({
                 <p className="text-sm text-[#8e8e8e]">&nbsp;</p>
               </div>
             </div>
-            <button
-              onClick={onChat}
-              className="flex items-center space-x-2 rounded-lg bg-[#3a3a3a] py-[6px] pr-2 pl-3"
-            >
-              <span className="body1 text-[#bdbebd]">채팅</span>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clip-path="url(#clip0_3562_10626)">
-                  <path
-                    d="M3.21379 2.72888C3.5142 2.68275 3.76174 2.76777 3.85246 2.7992C3.9815 2.84392 4.12899 2.9104 4.24211 2.96131L12.6005 6.72302C12.7109 6.77268 12.8567 6.83739 12.9745 6.90369C13.0711 6.95803 13.3491 7.11707 13.5116 7.44568C13.6843 7.79484 13.6843 8.20492 13.5116 8.55408C13.3491 8.88257 13.071 9.04175 12.9745 9.09607C12.8567 9.16237 12.7108 9.22709 12.6005 9.27673L4.24504 13.0365C4.13147 13.0876 3.98366 13.1548 3.85441 13.1996C3.75089 13.2355 3.44173 13.3417 3.08293 13.2416C2.70427 13.1359 2.39771 12.8578 2.2548 12.4916C2.11947 12.1447 2.19327 11.8268 2.21867 11.7201C2.25033 11.5871 2.30316 11.4339 2.34269 11.3158L3.44621 8.01697L2.33879 4.67908C2.29972 4.56138 2.24702 4.4086 2.21574 4.27576C2.19062 4.16897 2.11815 3.85133 2.25383 3.50525C2.39719 3.13971 2.7037 2.86252 3.08195 2.7572L3.21379 2.72888ZM3.49992 7.24939V8.74939H6.99992C7.41413 8.74939 7.74992 8.4136 7.74992 7.99939C7.74975 7.58533 7.41403 7.24939 6.99992 7.24939H3.49992Z"
-                    fill="#BDBEBD"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_3562_10626">
-                    <rect
-                      width="12"
-                      height="12"
-                      fill="white"
-                      transform="translate(2 2)"
-                    />
-                  </clipPath>
-                </defs>
-              </svg>
-            </button>
           </div>
 
           {/* Customer Photos */}
